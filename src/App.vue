0d28e95f576ed82b244b612777fa6e48de18104f
huangchengjie <597857074@qq.com>
2022-11-13 15:44:15 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const canvasEle = ref<HTMLCanvasElement>()
+
+onMounted(() => {
+	const context = canvasEle.value?.getContext('webgl2')!
+	context.clearColor(0, 0, 0, 1)
+	context.clear(context.COLOR_BUFFER_BIT)
+})
+</script>
+
+<template>
+	<canvas ref="canvasEle" width="800" height="800" />
+</template>
+
+<style scoped></style>
