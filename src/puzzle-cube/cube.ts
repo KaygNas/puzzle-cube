a07aae1b99d3248607304de7b6d9b5eb8dd7cab9
huangchengjie <597857074@qq.com>
2022-11-26 17:13:34 +0800

    feat: recolor cubes;

--- src/puzzle-cube/cube.ts
+++ src/puzzle-cube/cube.ts
@@ -1,9 +1,12 @@
 import { vec3, vec4 } from 'gl-matrix'
 import { Transform } from './transform'
 
-export type FaceName = 'front' | 'back' | 'up' | 'down' | 'left' | 'right'
+export const FACE_NAMES = ['up', 'front', 'left', 'down', 'back', 'right'] as const
+export type FaceName = typeof FACE_NAMES[number]
 
-export type FaceColor = 'red' | 'green' | 'blue' | 'yellow' | 'purple' | 'white' | 'black'
+export type FaceColor = 'red' | 'green' | 'blue' | 'yellow' | 'orange' | 'white' | 'black'
+
+export type CubeType = 'center' | 'edge' | 'corner'
 
 export const Color: Record<FaceColor, vec4> = {
   red: vec4.fromValues(192 / 255, 72 / 255, 81 / 255, 1),
@@ -11,32 +14,52 @@ export const Color: Record<FaceColor, vec4> = {
   blue: vec4.fromValues(26 / 255, 148 / 255, 188 / 255, 1),
   yellow: vec4.fromValues(228 / 255, 191 / 255, 17 / 255, 1),
   white: vec4.fromValues(251 / 255, 236 / 255, 222 / 255, 1),
-  purple: vec4.fromValues(129 / 255, 60 / 255, 133 / 255, 1),
+  orange: vec4.fromValues(248 / 255, 107 / 255, 29 / 255, 1),
   black: vec4.fromValues(79 / 255, 64 / 255, 50 / 255, 1),
 }
 
-
 const defaultFaceColors = (): Record<FaceName, FaceColor> => {
   return {
-    back: 'black',
-    down: 'black',
-    left: 'black',
-    front: 'black',
-    up: 'black',
-    right: 'black',
+    up: 'white',
+    front: 'red',
+    left: 'green',
+    down: 'orange',
+    back: 'yellow',
+    right: 'blue',
   }
 }
 
 export class Cube {
-  private _faceColors = defaultFaceColors()
+  faceColorNames = defaultFaceColors()
   get faceColors(): Record<FaceName, vec4> {
     return {
-      back: Color[this._faceColors.back],
-      down: Color[this._faceColors.down],
-      left: Color[this._faceColors.left],
-      front: Color[this._faceColors.front],
-      up: Color[this._faceColors.up],
-      right: Color[this._faceColors.right]
+      back: Color[this.faceColorNames.back],
+      down: Color[this.faceColorNames.down],
+      left: Color[this.faceColorNames.left],
+      front: Color[this.faceColorNames.front],
+      up: Color[this.faceColorNames.up],
+      right: Color[this.faceColorNames.right],
+    }
+  }
+
+  get faceNormals() {
+    const up = vec3.clone(this.up)
+    const front = vec3.clone(this.front)
+    const back = vec3.fromValues(0, 0, -1)
+    const down = vec3.fromValues(0, -1, 0)
+    const right = vec3.fromValues(1, 0, 0)
+    const left = vec3.fromValues(-1, 0, 0)
+    const localToWordMatrix = this.transform.localToWorld()
+    function toWorld(normal: vec3) {
+      return vec3.transformMat4(normal, normal, localToWordMatrix)
+    }
+    return {
+      up: toWorld(up),
+      front: toWorld(front),
+      back: toWorld(back),
+      down: toWorld(down),
+      right: toWorld(right),
+      left: toWorld(left),
     }
   }
 
@@ -44,11 +67,11 @@ export class Cube {
   public up: vec3 = vec3.fromValues(0, 1, 0)
   public size: number = 0.9
   public transform: Transform = new Transform()
+  public type: CubeType
 
-  constructor(
-    faceColors: Partial<Record<FaceName, FaceColor>>,
-    public center: vec3,
-  ) {
-    Object.assign(this._faceColors, faceColors)
+  constructor(public center: vec3, colorFaces: FaceName[]) {
+    this.type = colorFaces.length === 3 ? 'corner' : colorFaces.length === 2 ? 'edge' : 'center'
+    const uncolorFaces: FaceName[] = FACE_NAMES.filter((face) => !colorFaces.includes(face))
+    uncolorFaces.forEach((face) => (this.faceColorNames[face] = 'black'))
   }
 }
--- src/puzzle-cube/cubes.ts
+++ src/puzzle-cube/cubes.ts
@@ -4,42 +4,42 @@ import { Cube } from './cube'
 const center = vec3.fromValues
 
 export const cubes: Cube[] = [
-  // tier 1
-  new Cube({ back: 'red', down: 'green', left: 'blue', }, center(-1, -1, -1)),
-  new Cube({ back: 'red', down: 'green', }, center(0, -1, -1)),
-  new Cube({ back: 'red', down: 'green', right: 'purple', }, center(1, -1, -1)),
+	// tier 1
+	new Cube(center(-1, -1, -1), ['back', 'down', 'left']),
+	new Cube(center(0, -1, -1), ['back', 'down']),
+	new Cube(center(1, -1, -1), ['back', 'down', 'right']),
 
-  new Cube({ back: 'red', left: 'blue', }, center(-1, 0, -1)),
-  new Cube({ back: 'red', }, center(0, 0, -1)),
-  new Cube({ back: 'red', right: 'purple', }, center(1, 0, -1)),
+	new Cube(center(-1, -1, 0), ['down', 'left']),
+	new Cube(center(0, -1, 0), ['down']),
+	new Cube(center(1, -1, 0), ['down', 'right']),
 
-  new Cube({ back: 'red', up: 'white', left: 'blue', }, center(-1, 1, -1)),
-  new Cube({ back: 'red', up: 'white', }, center(0, 1, -1)),
-  new Cube({ back: 'red', up: 'white', right: 'purple', }, center(1, 1, -1)),
+	new Cube(center(-1, -1, 1), ['front', 'down', 'left']),
+	new Cube(center(0, -1, 1), ['front', 'down']),
+	new Cube(center(1, -1, 1), ['front', 'down', 'right']),
 
-  // tier 2
-  new Cube({ down: 'green', left: 'blue', }, center(-1, -1, 0)),
-  new Cube({ down: 'green', }, center(0, -1, 0)),
-  new Cube({ down: 'green', right: 'purple', }, center(1, -1, 0)),
+	// tier 2
+	new Cube(center(-1, 0, -1), ['back', 'left']),
+	new Cube(center(0, 0, -1), ['back']),
+	new Cube(center(1, 0, -1), ['back', 'right']),
 
-  new Cube({ left: 'blue', }, center(-1, 0, 0)),
-  new Cube({}, center(0, 0, 0)),
-  new Cube({ right: 'purple', }, center(1, 0, 0)),
+	new Cube(center(-1, 0, 0), ['left']),
+	new Cube(center(0, 0, 0), []),
+	new Cube(center(1, 0, 0), ['right']),
 
-  new Cube({ up: 'white', left: 'blue', }, center(-1, 1, 0)),
-  new Cube({ up: 'white', }, center(0, 1, 0)),
-  new Cube({ up: 'white', right: 'purple', }, center(1, 1, 0)),
+	new Cube(center(-1, 0, 1), ['front', 'left']),
+	new Cube(center(0, 0, 1), ['front']),
+	new Cube(center(1, 0, 1), ['front', 'right']),
 
-  // tier 3
-  new Cube({ front: 'yellow', down: 'green', left: 'blue', }, center(-1, -1, 1)),
-  new Cube({ front: 'yellow', down: 'green', }, center(0, -1, 1)),
-  new Cube({ front: 'yellow', down: 'green', right: 'purple', }, center(1, -1, 1)),
+	// tier 3
+	new Cube(center(-1, 1, -1), ['back', 'up', 'left']),
+	new Cube(center(0, 1, -1), ['back', 'up']),
+	new Cube(center(1, 1, -1), ['back', 'up', 'right']),
 
-  new Cube({ front: 'yellow', left: 'blue', }, center(-1, 0, 1)),
-  new Cube({ front: 'yellow', }, center(0, 0, 1)),
-  new Cube({ front: 'yellow', right: 'purple', }, center(1, 0, 1)),
+	new Cube(center(-1, 1, 0), ['up', 'left']),
+	new Cube(center(0, 1, 0), ['up']),
+	new Cube(center(1, 1, 0), ['up', 'right']),
 
-  new Cube({ front: 'yellow', up: 'white', left: 'blue', }, center(-1, 1, 1)),
-  new Cube({ front: 'yellow', up: 'white', }, center(0, 1, 1)),
-  new Cube({ front: 'yellow', up: 'white', right: 'purple', }, center(1, 1, 1)),
+	new Cube(center(-1, 1, 1), ['front', 'up', 'left']),
+	new Cube(center(0, 1, 1), ['front', 'up']),
+	new Cube(center(1, 1, 1), ['front', 'up', 'right']),
 ]
