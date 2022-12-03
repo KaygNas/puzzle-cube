f6d8c86d97925334f926605389c535454ab0144e
huangchengjie <597857074@qq.com>
2022-12-03 17:57:39 +0800

    refactor: use beam-gl to render cube;

--- src/puzzle-cube/cube-renderer/cube-renderer.ts
+++ src/puzzle-cube/cube-renderer/cube-renderer.ts
@@ -1,98 +1,98 @@
 import { Cube } from '../cube'
-import {
-  getGl,
-  GL,
-  initIndicesBuffer,
-  initShader,
-  initVertexBuffers,
-  setLight,
-  setMatrix,
-} from './webgl-utils'
 import VERTEX_SHADER from './vertex-shader.glsl'
 import FRAGMENT_SHADER from './fragment-shader.glsl'
-import { vec3, vec4, mat4 } from 'gl-matrix'
+import { vec3, mat4 } from 'gl-matrix'
 import { CubeVerticesWriter } from './cube-vertices-writer'
-
-// TODO: use Beam to render
-
+import { Beam, ResourceTypes, SchemaTypes, Shader } from 'beam-gl'
 export class CubeRenderer {
-  private gl: GL
+	private beam: Beam
+	private shader: Shader
 
-  private viewMat = mat4.create()
-  private eye = vec3.fromValues(-3, 3, 7)
-  private center = vec3.fromValues(0.0, 0.0, 0.0)
-  private up = vec3.fromValues(0.0, 1.0, 0.0)
-  private modelMat = mat4.create()
-  private projMat = mat4.ortho(mat4.create(), -5, 5, -5, 5, 1, 100)
+	private viewMat = mat4.create()
+	private eye = vec3.fromValues(-3, 3, 7)
+	private center = vec3.fromValues(0.0, 0.0, 0.0)
+	private up = vec3.fromValues(0.0, 1.0, 0.0)
+	private modelMat = mat4.create()
+	private projMat = mat4.ortho(mat4.create(), -5, 5, -5, 5, 1, 100)
 
-  cubes = new Set<Cube>()
+	cubes = new Set<Cube>()
 
-  constructor(private canvasElement: HTMLCanvasElement) {
-    const gl = getGl(this.canvasElement)
-    initShader(gl, VERTEX_SHADER, FRAGMENT_SHADER)
-    setLight(gl, [
-      { uniformName: 'u_LightColor', vector: vec3.fromValues(1, 1, 1) },
-      { uniformName: 'u_AmbientLight', vector: vec3.fromValues(0.6, 0.6, 0.6) },
-      { uniformName: 'u_LightPosition', vector: vec3.fromValues(-3.0, 7.0, 8.0) },
-    ])
-    gl.enable(gl.DEPTH_TEST)
-    this.enableRotationControl()
-    this.gl = gl
-  }
+	constructor(private canvasElement: HTMLCanvasElement) {
+		this.beam = new Beam(canvasElement)
+		this.shader = this.beam.shader({
+			fs: FRAGMENT_SHADER,
+			vs: VERTEX_SHADER,
+			buffers: {
+				a_Position: { type: SchemaTypes.vec4 },
+				a_Color: { type: SchemaTypes.vec4 },
+				a_Normal: { type: SchemaTypes.vec4 },
+			},
+			uniforms: {
+				u_LightColor: { type: SchemaTypes.vec3, default: vec3.fromValues(1, 1, 1) },
+				u_AmbientLight: { type: SchemaTypes.vec3, default: vec3.fromValues(0.6, 0.6, 0.6) },
+				u_LightPosition: { type: SchemaTypes.vec3, default: vec3.fromValues(-3.0, 7.0, 8.0) },
+				u_ViewMatrix: { type: SchemaTypes.mat4, default: this.viewMat },
+				u_ProjMatrix: { type: SchemaTypes.mat4, default: this.projMat },
+				u_ModelMatrix: { type: SchemaTypes.mat4, default: this.modelMat },
+			},
+		})
+		const gl = this.beam.gl
+		gl.enable(gl.DEPTH_TEST)
+		this.enableRotationControl()
+	}
 
-  add(cube: Cube) {
-    this.cubes.add(cube)
-  }
+	add(cube: Cube) {
+		this.cubes.add(cube)
+	}
 
-  render() {
-    const { gl, viewMat, eye, center, up, projMat } = this
-    mat4.lookAt(viewMat, eye, center, up)
-    setMatrix(gl, 'u_ViewMatrix', viewMat)
-    setMatrix(gl, 'u_ProjMatrix', projMat)
+	render() {
+		const { viewMat, eye, center, up } = this
+		mat4.lookAt(viewMat, eye, center, up)
+		this.cubes.forEach((cube) => this.renderCube(cube))
+	}
 
-    gl.clear(gl.DEPTH_BUFFER_BIT)
-    gl.clear(gl.COLOR_BUFFER_BIT)
-    this.cubes.forEach(cube => this.renderCube(cube))
-  }
+	private renderCube(cube: Cube): void {
+		const { modelMat, beam, shader, viewMat, projMat } = this
+		const { vertices, indices } = CubeVerticesWriter.toVertices(cube)
+		const _modelMat = mat4.multiply(mat4.create(), modelMat, cube.transform.localToWorld())
+		beam.draw(
+			shader,
+			beam.resource(ResourceTypes.VertexBuffers, {
+				a_Position: vertices.position,
+				a_Color: vertices.color,
+				a_Normal: vertices.normal,
+			}),
+			beam.resource(ResourceTypes.IndexBuffer, { array: indices }),
+			beam.resource(ResourceTypes.Uniforms, {
+				u_ModelMatrix: _modelMat,
+				u_ViewMatrix: viewMat,
+				u_ProjMatrix: projMat,
+			}),
+		)
+	}
 
-  private renderCube(cube: Cube): void {
-    const { gl, modelMat } = this
-    const { vertices, indices, VERTEX_TOTAL_SIZE } = CubeVerticesWriter.toVertices(cube)
-    initVertexBuffers(gl, vertices, VERTEX_TOTAL_SIZE, [
-      { attributeName: 'a_Position', size: 4, offset: 0 },
-      { attributeName: 'a_Color', size: 4, offset: 4 },
-      { attributeName: 'a_Normal', size: 4, offset: 8 },
-    ])
-    initIndicesBuffer(gl, indices)
-
-    const _modelMat = mat4.multiply(mat4.create(), modelMat, cube.transform.localToWorld())
-    setMatrix(gl, 'u_ModelMatrix', _modelMat)
-
-    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0)
-  }
-
-  private enableRotationControl() {
-    const onkeydown = (e: KeyboardEvent) => {
-      const { modelMat } = this
-      const tranformMat = mat4.create()
-      switch (e.key) {
-        case 'ArrowLeft':
-          mat4.fromRotation(tranformMat, 0.1, vec3.fromValues(0.0, 1.0, 0.0))
-          break
-        case 'ArrowRight':
-          mat4.fromRotation(tranformMat, -0.1, vec3.fromValues(0.0, 1.0, 0.0))
-          break
-        case 'ArrowUp':
-          mat4.fromRotation(tranformMat, 0.1, vec3.fromValues(1.0, 0.0, 0.0))
-          break
-        case 'ArrowDown':
-          mat4.fromRotation(tranformMat, -0.1, vec3.fromValues(1.0, 0.0, 0.0))
-          break
-        default:
-      }
-      mat4.multiply(modelMat, modelMat, tranformMat)
-      this.render()
-    }
-    window.addEventListener('keydown', onkeydown)
-  }
+	private enableRotationControl() {
+		const onkeydown = (e: KeyboardEvent) => {
+			const { modelMat } = this
+			const tranformMat = mat4.create()
+			switch (e.key) {
+				case 'ArrowLeft':
+					mat4.fromRotation(tranformMat, 0.1, vec3.fromValues(0.0, 1.0, 0.0))
+					break
+				case 'ArrowRight':
+					mat4.fromRotation(tranformMat, -0.1, vec3.fromValues(0.0, 1.0, 0.0))
+					break
+				case 'ArrowUp':
+					mat4.fromRotation(tranformMat, 0.1, vec3.fromValues(1.0, 0.0, 0.0))
+					break
+				case 'ArrowDown':
+					mat4.fromRotation(tranformMat, -0.1, vec3.fromValues(1.0, 0.0, 0.0))
+					break
+				default:
+			}
+			mat4.multiply(modelMat, modelMat, tranformMat)
+			this.render()
+		}
+		window.addEventListener('keydown', onkeydown)
+	}
 }
--- src/puzzle-cube/cube-renderer/cube-vertices-writer.ts
+++ src/puzzle-cube/cube-renderer/cube-vertices-writer.ts
@@ -1,170 +1,163 @@
-import { vec3, vec4 } from "gl-matrix"
-import { Cube } from "../cube"
+import { vec3, vec4 } from 'gl-matrix'
+import { Cube } from '../cube'
 
-const VERTEX_SIZE = 4
-const COLOR_SIZE = 4
-const NORMAL_SIZE = 4
 const VERTICES_COUNT_PER_FACE = 4
 const FACE_COUNT = 6
-const VERTEX_TOTAL_SIZE = VERTEX_SIZE + COLOR_SIZE + NORMAL_SIZE
 
 const indices = createIndices()
 
 function createIndices() {
-  const INDICES_COUNT = 3 * 2 * FACE_COUNT
-  const indices = new Uint8Array(INDICES_COUNT)
-  let j = 0
-  for (let i = 0; i < FACE_COUNT; i++) {
-    indices[j++] = i * 4
-    indices[j++] = i * 4 + 1
-    indices[j++] = i * 4 + 2
-    indices[j++] = i * 4
-    indices[j++] = i * 4 + 2
-    indices[j++] = i * 4 + 3
-  }
-  return indices
+	const INDICES_COUNT = 3 * 2 * FACE_COUNT
+	const indices = new Uint16Array(INDICES_COUNT)
+	let j = 0
+	for (let i = 0; i < FACE_COUNT; i++) {
+		indices[j++] = i * 4
+		indices[j++] = i * 4 + 1
+		indices[j++] = i * 4 + 2
+		indices[j++] = i * 4
+		indices[j++] = i * 4 + 2
+		indices[j++] = i * 4 + 3
+	}
+	return indices
 }
 
-function createEmptyVertices() {
-  return new Float32Array(
-    VERTEX_TOTAL_SIZE * VERTICES_COUNT_PER_FACE * FACE_COUNT,
-  )
+function createEmptyArray() {
+	return new Float32Array(VERTICES_COUNT_PER_FACE * FACE_COUNT * 4)
 }
 
 export class CubeVerticesWriter {
-  indices = indices
-  vertices = createEmptyVertices()
-  VERTEX_TOTAL_SIZE = VERTEX_TOTAL_SIZE
+	indices = indices
+	vertices = {
+		position: createEmptyArray(),
+		color: createEmptyArray(),
+		normal: createEmptyArray(),
+	}
+	private currIndex = {
+		position: 0,
+		color: 0,
+		normal: 0,
+	}
 
-  private currIndex = 0
+	static toVertices(cube: Cube) {
+		const writer = new CubeVerticesWriter()
+		writer.writeVertices(cube)
+		return writer
+	}
 
-  static toVertices(cube: Cube) {
-    const writer = new CubeVerticesWriter()
-    writer.writeVertices(cube)
-    return writer
-  }
+	private writeVertices(cube: Cube) {
+		const { vertices } = this
+		const cubeFront = cube.front
+		const cubeUp = cube.up
+		const cubeLeft = vec3.cross(vec3.create(), cubeFront, cubeUp)
+		const cubeBack = vec3.scale(vec3.create(), cubeFront, -1)
+		const cubeDown = vec3.scale(vec3.create(), cubeUp, -1)
+		const cubeRight = vec3.scale(vec3.create(), cubeLeft, -1)
 
-  private writeVertices(cube: Cube) {
-    const { vertices } = this
-    const cubeFront = cube.front
-    const cubeUp = cube.up
-    const cubeLeft = vec3.cross(vec3.create(), cubeFront, cubeUp)
-    const cubeBack = vec3.scale(vec3.create(), cubeFront, -1)
-    const cubeDown = vec3.scale(vec3.create(), cubeUp, -1)
-    const cubeRight = vec3.scale(vec3.create(), cubeLeft, -1)
+		this.writeFaceVertices({
+			color: cube.faceColors.front,
+			cubeCenter: cube.center,
+			cubeSize: cube.size,
+			toCenter: cubeFront,
+			toTop: cubeUp,
+			toLeft: cubeLeft,
+		})
+		this.writeFaceVertices({
+			color: cube.faceColors.back,
+			cubeCenter: cube.center,
+			cubeSize: cube.size,
+			toCenter: cubeBack,
+			toTop: cubeUp,
+			toLeft: cubeLeft,
+		})
+		this.writeFaceVertices({
+			color: cube.faceColors.up,
+			cubeCenter: cube.center,
+			cubeSize: cube.size,
+			toCenter: cubeUp,
+			toTop: cubeBack,
+			toLeft: cubeLeft,
+		})
+		this.writeFaceVertices({
+			color: cube.faceColors.down,
+			cubeCenter: cube.center,
+			cubeSize: cube.size,
+			toCenter: cubeDown,
+			toTop: cubeBack,
+			toLeft: cubeLeft,
+		})
+		this.writeFaceVertices({
+			color: cube.faceColors.left,
+			cubeCenter: cube.center,
+			cubeSize: cube.size,
+			toCenter: cubeLeft,
+			toTop: cubeUp,
+			toLeft: cubeFront,
+		})
+		this.writeFaceVertices({
+			color: cube.faceColors.right,
+			cubeCenter: cube.center,
+			cubeSize: cube.size,
+			toCenter: cubeRight,
+			toTop: cubeUp,
+			toLeft: cubeFront,
+		})
 
-    this.writeFaceVertices({
-      color: cube.faceColors.front,
-      cubeCenter: cube.center,
-      cubeSize: cube.size,
-      toCenter: cubeFront,
-      toTop: cubeUp,
-      toLeft: cubeLeft,
-    })
-    this.writeFaceVertices({
-      color: cube.faceColors.back,
-      cubeCenter: cube.center,
-      cubeSize: cube.size,
-      toCenter: cubeBack,
-      toTop: cubeUp,
-      toLeft: cubeLeft,
-    })
-    this.writeFaceVertices({
-      color: cube.faceColors.up,
-      cubeCenter: cube.center,
-      cubeSize: cube.size,
-      toCenter: cubeUp,
-      toTop: cubeBack,
-      toLeft: cubeLeft,
-    })
-    this.writeFaceVertices({
-      color: cube.faceColors.down,
-      cubeCenter: cube.center,
-      cubeSize: cube.size,
-      toCenter: cubeDown,
-      toTop: cubeBack,
-      toLeft: cubeLeft,
-    })
-    this.writeFaceVertices({
-      color: cube.faceColors.left,
-      cubeCenter: cube.center,
-      cubeSize: cube.size,
-      toCenter: cubeLeft,
-      toTop: cubeUp,
-      toLeft: cubeFront,
-    })
-    this.writeFaceVertices({
-      color: cube.faceColors.right,
-      cubeCenter: cube.center,
-      cubeSize: cube.size,
-      toCenter: cubeRight,
-      toTop: cubeUp,
-      toLeft: cubeFront,
-    })
+		return vertices
+	}
 
-    return vertices
-  }
+	private writeFaceVertices(face: {
+		cubeCenter: vec3
+		cubeSize: number
+		toCenter: vec3
+		toTop: vec3
+		toLeft: vec3
+		color: vec4
+	}) {
+		const center = vec3.clone(face.cubeCenter)
+		const leftTop = vec3.create()
+		const rightTop = vec3.create()
+		const rightBottom = vec3.create()
+		const leftBottom = vec3.create()
+		const halfSideLength = 0.5 * face.cubeSize
 
-  private writeFaceVertices(
-    face: {
-      cubeCenter: vec3
-      cubeSize: number
-      toCenter: vec3
-      toTop: vec3
-      toLeft: vec3
-      color: vec4
-    },
-  ) {
-    const center = vec3.clone(face.cubeCenter)
-    const leftTop = vec3.create()
-    const rightTop = vec3.create()
-    const rightBottom = vec3.create()
-    const leftBottom = vec3.create()
-    const halfSideLength = 0.5 * face.cubeSize
+		vec3.scaleAndAdd(center, center, face.toCenter, halfSideLength)
 
-    vec3.scaleAndAdd(center, center, face.toCenter, halfSideLength)
+		vec3.scaleAndAdd(leftTop, center, face.toLeft, halfSideLength)
+		vec3.scaleAndAdd(leftTop, leftTop, face.toTop, halfSideLength)
 
-    vec3.scaleAndAdd(leftTop, center, face.toLeft, halfSideLength)
-    vec3.scaleAndAdd(leftTop, leftTop, face.toTop, halfSideLength)
+		vec3.scaleAndAdd(rightTop, center, face.toLeft, -halfSideLength)
+		vec3.scaleAndAdd(rightTop, rightTop, face.toTop, halfSideLength)
 
-    vec3.scaleAndAdd(rightTop, center, face.toLeft, -halfSideLength)
-    vec3.scaleAndAdd(rightTop, rightTop, face.toTop, halfSideLength)
+		vec3.scaleAndAdd(rightBottom, center, face.toLeft, -halfSideLength)
+		vec3.scaleAndAdd(rightBottom, rightBottom, face.toTop, -halfSideLength)
 
-    vec3.scaleAndAdd(rightBottom, center, face.toLeft, -halfSideLength)
-    vec3.scaleAndAdd(rightBottom, rightBottom, face.toTop, -halfSideLength)
+		vec3.scaleAndAdd(leftBottom, center, face.toLeft, halfSideLength)
+		vec3.scaleAndAdd(leftBottom, leftBottom, face.toTop, -halfSideLength)
 
-    vec3.scaleAndAdd(leftBottom, center, face.toLeft, halfSideLength)
-    vec3.scaleAndAdd(leftBottom, leftBottom, face.toTop, -halfSideLength)
+		this.writeVertex(leftTop, face.color, face.toCenter)
+		this.writeVertex(rightTop, face.color, face.toCenter)
+		this.writeVertex(rightBottom, face.color, face.toCenter)
+		this.writeVertex(leftBottom, face.color, face.toCenter)
+	}
 
+	private writeVertex(position: vec3, color: vec4, normal: vec3) {
+		const { vertices, currIndex } = this
+		let index = currIndex
 
-    this.writeVertex(leftTop, face.color, face.toCenter)
-    this.writeVertex(rightTop, face.color, face.toCenter)
-    this.writeVertex(rightBottom, face.color, face.toCenter)
-    this.writeVertex(leftBottom, face.color, face.toCenter)
-  }
+		position.forEach((value) => {
+			vertices.position[index.position++] = value
+		})
+		vertices.position[index.position++] = 1
 
-  private writeVertex(
-    vertex: vec3,
-    color: vec4,
-    normal: vec3,
-  ) {
-    const { vertices, currIndex } = this
-    let index = currIndex
+		color.forEach((value) => {
+			vertices.color[index.color++] = value
+		})
 
-    vertex.forEach((value) => {
-      vertices[index++] = value
-    })
-    vertices[index++] = 1
+		normal.forEach((value) => {
+			vertices.normal[index.normal++] = value
+		})
+		vertices.normal[index.normal++] = 1
 
-    color.forEach((value) => {
-      vertices[index++] = value
-    })
-
-    normal.forEach(value => {
-      vertices[index++] = value
-    })
-    vertices[index++] = 1
-
-    this.currIndex = index
-  }
+		this.currIndex = index
+	}
 }
